9f5dc07b3615dc193891a15c7561c6823d49bcef
Mahfujur51 <overcastmoon@gmail.com>
2020-05-14 00:01:12 +0600

    product insert successuflly!!

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -1,91 +1,91 @@
-  <?php
+<?php
 session_start();
 include('include/db.php');
 if(strlen($_SESSION['alogin'])==0)
-  {
+{
   header('location:login.php');
 }
 else{
-if(isset($_POST['btn_save']))
-{
-$product_name=$_POST['product_name'];
-$details=$_POST['details'];
-$price=$_POST['price'];
-$c_price=$_POST['c_price'];
-$product_type=$_POST['product_type'];
-$brand=$_POST['brand'];
-$tags=$_POST['tags'];
+  if(isset($_POST['btn_save']))
+  {
+    $product_title=$_POST['product_title'];
+    $product_desc=$_POST['product_desc'];
+    $product_price=$_POST['product_price'];
+    $brandid=$_POST['brandid'];
+    $catid=$_POST['catid'];
 
+    $product_keyword=$_POST['product_keyword'];
 //picture coding
-$picture_name=$_FILES['picture']['name'];
-$picture_type=$_FILES['picture']['type'];
-$picture_tmp_name=$_FILES['picture']['tmp_name'];
-$picture_size=$_FILES['picture']['size'];
+    $picture_name=$_FILES['product_image']['name'];
+    $picture_type=$_FILES['product_image']['type'];
+    $picture_tmp_name=$_FILES['product_image']['tmp_name'];
+    $picture_size=$_FILES['product_image']['size'];
+    if($picture_type=="image/jpeg" || $picture_type=="image/jpg" || $picture_type=="image/png" || $picture_type=="image/gif")
+    {
+      if($picture_size<=50000000)
 
-if($picture_type=="image/jpeg" || $picture_type=="image/jpg" || $picture_type=="image/png" || $picture_type=="image/gif")
-{
-	if($picture_size<=50000000)
-	
-		$pic_name=time()."_".$picture_name;
-		move_uploaded_file($picture_tmp_name,"../product_images/".$pic_name);
-		
-mysqli_query($con,"insert into products (product_cat, product_brand,product_title,product_price, product_desc, product_image,product_keywords) values ('$product_type','$brand','$product_name','$price','$details','$pic_name','$tags')") or die ("query incorrect");
+        $pic_name=time()."_".$picture_name;
+      move_uploaded_file($picture_tmp_name,"../product_images/".$pic_name);
+      $sql="INSERT INTO tbl_product(catid,brandid,product_title,product_price,product_desc,product_image,product_keyword)VALUES('$catid','$brandid','$product_title','$product_price','$product_desc','$pic_name','$product_keyword')";
+      $query=mysqli_query($con,$sql);
+      if ($query) {
+        echo "<script>alert('Product Inserted Successfully')</script>";
 
- header("location: sumit_form.php?success=1");
-}
+      }else{
+         echo "<script>alert('Product Not Inserted Successfully')</script>";
+      }
 
-mysqli_close($con);
-}
-include "sidenav.php";
-include "topheader.php";
-?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-          <form action="" method="post" type="form" name="form" enctype="multipart/form-data">
-          <div class="row">
-          
-                
-         <div class="col-md-7">
+  } }
+  include "sidenav.php";
+  include "topheader.php";
+  ?>
+  <!-- End Navbar -->
+  <div class="content">
+    <div class="container-fluid">
+      <form action="" method="post" type="form" name="form" enctype="multipart/form-data">
+        <div class="row">
+
+
+          <div class="col-md-7">
             <div class="card">
               <div class="card-header card-header-primary">
                 <h5 class="title">Add Product</h5>
               </div>
               <div class="card-body">
-                
-                  <div class="row">
-                    
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Product Title</label>
-                        <input type="text" id="product_name" required name="product_name" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-md-4">
-                      <div class="">
-                        <label for="">Add Image</label>
-                        <input type="file" name="picture" required class="btn btn-fill btn-success" id="picture" >
-                      </div>
-                    </div>
-                     <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Description</label>
-                        <textarea rows="4" cols="80" id="details" required name="details" class="form-control"></textarea>
-                      </div>
-                    </div>
-                  
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Pricing</label>
-                        <input type="text" id="price" name="price" required class="form-control" >
-                      </div>
+
+                <div class="row">
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Product Title</label>
+                      <input type="text" id="product_name" required name="product_title" class="form-control">
                     </div>
                   </div>
-                 
-                  
-                
+                  <div class="col-md-4">
+                    <div class="">
+                      <label for="">Add Image</label>
+                      <input type="file" name="product_image" required class="btn btn-fill btn-success" id="picture" >
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Description</label>
+                      <textarea rows="4" cols="80" id="details" required name="product_desc" class="form-control"></textarea>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Pricing</label>
+                      <input type="text" id="price" name="product_price" required class="form-control" >
+                    </div>
+                  </div>
+                </div>
+
+
+
               </div>
-              
+
             </div>
           </div>
           <div class="col-md-5">
@@ -94,44 +94,68 @@ include "topheader.php";
                 <h5 class="title">Categories</h5>
               </div>
               <div class="card-body">
-                
-                  <div class="row">
-                    
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Product Category</label>
-                        <input type="number" id="product_type" name="product_type" required="[1-6]" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label for="">Product Brand</label>
-                        <input type="number" id="brand" name="brand" required class="form-control">
-                      </div>
-                    </div>
-                     
-                  
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Product Keywords</label>
-                        <input type="text" id="tags" name="tags" required class="form-control" >
-                      </div>
+
+                <div class="row">
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Product Category</label>
+                      <select name="catid" class="form-control">
+                        <option>Select Category</option>
+                        <?php
+                        $sql="SELECT * FROM tbl_category";
+                        $query=mysqli_query($con,$sql);
+                        while ($result=mysqli_fetch_array($query)) {
+                      # code...
+                          ?>
+                          <option value="<?php echo $result['id'] ?>"><?php echo $result['catname']; ?></option>
+                          <?php
+                        }
+                        ?>
+                      </select>
                     </div>
                   </div>
-                
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="">Product Brand</label>
+                     <select name="brandid" class="form-control">
+                        <option>Select Brand</option>
+                        <?php
+                        $sql="SELECT * FROM tbl_brand";
+                        $query=mysqli_query($con,$sql);
+                        while ($result=mysqli_fetch_array($query)) {
+                      # code...
+                          ?>
+                          <option value="<?php echo $result['id'] ?>"><?php echo $result['brandname']; ?></option>
+                          <?php
+                        }
+                        ?>
+                      </select>
+                    </div>
+                  </div>
+
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Product Keywords</label>
+                      <input type="text" name="product_keyword" required class="form-control" >
+                    </div>
+                  </div>
+                </div>
+
               </div>
               <div class="card-footer">
-                  <button type="submit" id="btn_save" name="btn_save" required class="btn btn-fill btn-primary">Update Product</button>
+                <button type="submit" id="btn_save" name="btn_save" required class="btn btn-fill btn-primary">Update Product</button>
               </div>
             </div>
           </div>
-          
+
         </div>
-         </form>
-          
-        </div>
-      </div>
-      <?php
-include "footer.php";
+      </form>
+
+    </div>
+  </div>
+  <?php
+  include "footer.php";
 }
 ?>
--- admin/productlist.php
+++ admin/productlist.php
@@ -1,123 +1,136 @@
- <?php
+<?php
 session_start();
-include('include/config.php');
+include('include/db.php');
 if(strlen($_SESSION['alogin'])==0)
-  {
+{
   header('location:login.php');
 }else{
-  if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='delete')
-{
-$product_id=$_GET['product_id'];
+  if(isset($_GET['del']))
+  {
+    $id=$_GET['del'];
 ///////picture delete/////////
-$result=mysqli_query($con,"select product_image from products where product_id='$product_id'")
-or die("query is incorrect...");
+    $result=mysqli_query($con,"SELECT product_image FROM tbl_product WHERE id='$id'");
+    list($picture)=mysqli_fetch_array($result);
+    $path="../product_images/$picture";
+    if(file_exists($path)==true)
+    {
+      unlink($path);
+    }
+    else
+    {}
+  /*this is delet query*/
+  $sql="DELETE FROM tbl_product WHERE id='$id'";
+  $query=mysqli_query($con,$sql);
+  if ($query) {
+    echo "<script>alert('Product Deleted Successfully')</script>";
+  }
 
-list($picture)=mysqli_fetch_array($result);
-$path="../product_images/$picture";
-
-if(file_exists($path)==true)
-{
-  unlink($path);
 }
-else
-{}
-/*this is delet query*/
-mysqli_query($con,"delete from products where product_id='$product_id'")or die("query is incorrect...");
-}
-
 ///pagination
-
 $page=$_GET['page'];
-
 if($page=="" || $page=="1")
 {
-$page1=0;	
+  $page1=0;
 }
 else
 {
-$page1=($page*10)-10;	
-} 
+  $page1=($page*10)-10;
+}
 include "sidenav.php";
 include "topheader.php";
 ?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-        
-        
-         <div class="col-md-14">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title"> Products List</h4>
-                
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table tablesorter " id="page1">
-                    <thead class=" text-primary">
-                      <tr><th>Image</th><th>Name</th><th>Price</th><th>
-	<a class=" btn btn-primary" href="addproduct.php">Add New</a></th></tr></thead>
-                    <tbody>
-                      <?php 
+<!-- End Navbar -->
+<div class="content">
+  <div class="container-fluid">
 
-                        $result=mysqli_query($con,"select product_id,product_image, product_title,product_price from products  where  product_cat=2 or product_cat=3 or product_cat=4 Limit $page1,12")or die ("query 1 incorrect.....");
 
-                        while(list($product_id,$image,$product_name,$price)=mysqli_fetch_array($result))
-                        {
-                        echo "<tr><td><img src='../product_images/$image' style='width:50px; height:50px; border:groove #000'></td><td>$product_name</td>
-                        <td>$price</td>
-                        <td>
-
-                        <a class=' btn btn-success' href='clothes_list.php?product_id=$product_id&action=delete'>Delete</a>
-                        </td></tr>";
-                        }
-
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
-            <nav aria-label="Page navigation example">
-              <ul class="pagination">
-                <li class="page-item">
-                  <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                    <span class="sr-only">Previous</span>
-                  </a>
-                </li>
-                 <?php 
-//counting paging
-
-                $paging=mysqli_query($con,"select product_id,product_image, product_title,product_price from products");
-                $count=mysqli_num_rows($paging);
-
-                $a=$count/10;
-                $a=ceil($a);
-                
-                for($b=1; $b<=$a;$b++)
-                {
-                ?> 
-                <li class="page-item"><a class="page-link" href="productlist.php?page=<?php echo $b;?>"><?php echo $b." ";?></a></li>
-                <?php	
-}
-?>
-                <li class="page-item">
-                  <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                    <span class="sr-only">Next</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-            
-           
-
-          </div>
-          
+    <div class="col-md-14">
+      <div class="card ">
+        <div class="card-header card-header-primary">
+          <h4 class="card-title"> Products List</h4>
           
         </div>
+        <div class="card-body">
+          <div class="table-responsive ps">
+            <table class="table tablesorter " id="page1">
+              <thead class=" text-primary">
+                <tr>
+                  <th>SlNo.</th>
+                  <th>Image</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>
+                    <a class=" btn btn-primary" href="addproduct.php">Add New</a>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php 
+                $sql="SELECT * FROM tbl_product";
+                $query=mysqli_query($con,$sql);
+                $cont=1;
+                while ($result=mysqli_fetch_array($query)) {
+                    
+                 ?>
+                 <tr>
+                  <td><?php echo $cont; ?></td>
+                  <td><img src="../product_images/<?php echo $result['product_image']; ?>" alt="" height="100" width="100"></td>
+                  <td><?php echo $result['product_title']; ?></td>
+                  <td> <?php echo $result['product_price']; ?></td>
+                  <td> <a href="?del=<?php echo $result['id'];?>" class="btn btn-danger" onclick="return confirm('Are You sure to Delete!!')">Delete</a></td>
+                  
+
+
+                </tr>
+                <?php 
+                $cont++;
+              }
+              ?>
+
+
+
+            </tbody>
+          </table>
+          <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+        </div>
       </div>
-      <?php
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+              <span class="sr-only">Previous</span>
+            </a>
+          </li>
+          <?php
+              //counting paging
+          $paging=mysqli_query($con,"SELECT * from tbl_product");
+          $count=mysqli_num_rows($paging);
+          $a=$count/10;
+          $a=ceil($a);
+
+          for($b=1; $b<=$a;$b++)
+          {
+            ?>
+            <li class="page-item"><a class="page-link" href="productlist.php?page=<?php echo $b;?>"><?php echo $b." ";?></a></li>
+            <?php
+          }
+          ?>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+              <span class="sr-only">Next</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+
+    </div>
+
+
+  </div>
+</div>
+<?php
 include "footer.php"; }
 ?>
